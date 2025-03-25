c02e9e357cd553636a92a5385018bf364192187f
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-03-25 18:07:18 -0400

    code

---
+++ code.c
@@ -0,0 +1,34 @@
+/*
+ * Created by Liya G.
+ * Created on March 25, 2025
+ 
+ * Uses a sonar
+ */
+
+ const int TRIG_PIN = 3;
+ const int ECHO_PIN = 2;
+ const float SPEED_OF_LIGHT = 0.343
+ 
+ float duration, distance;
+   
+  void setup() 
+ {
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    Serial.begin(9600);
+  }
+  
+  void loop() 
+ {
+    digitalWrite(TRIG_PIN, LOW);  
+    delayMicroseconds(2);  
+    digitalWrite(TRIG_PIN, HIGH);  
+    delayMicroseconds(10);  
+    digitalWrite(TRIG_PIN, LOW); 
+    
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration*SPEED_OF_LIGHT)/2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    delay(100);
+  }
